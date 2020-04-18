b9fbc7508168bb48d32d71e592518ec89f23599d
Luan Andrade <luansandrade92@hotmail.com>
2020-04-18 16:06:41 -0300

    valores entre -1 e 1 em Purchase apos padronizacao

--- data-science-0/main.py
+++ data-science-0/main.py
@@ -165,12 +165,18 @@ q8()
 # 
 # Quantas ocorrências entre -1 e 1 inclusive existem da variáel `Purchase` após sua padronização? Responda como um único escalar.
 
-# In[12]:
+# In[30]:
 
 
 def q9():
-    # Retorne aqui o resultado da questão 9.
-    pass
+    purchase = black_friday.Purchase
+    purchase_st = (purchase - purchase.mean())                     / (purchase.max() - purchase.min())
+    
+    conditions = (purchase_st >= -1) & (purchase_st <= 1)
+    return int(purchase_st[conditions].count())
+    
+
+q9()
 
 
 # ## Questão 10
